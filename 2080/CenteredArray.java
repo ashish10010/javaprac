60288271ed1f401baa0cf1da2ff890896893ad6e
Ashish Kunwar <thaladipesh89@gmail.com>
2024-12-27 22:38:44 +0545

    qn1 second part

---
+++ 2080/CenteredArray.java
@@ -0,0 +1,25 @@
+
+public class CenteredArray {
+
+    public static int isCentered(int[] arr) 
+    {
+        if(arr.length % 2 == 0) return 0;
+
+        int middleIndex = arr.length / 2;
+        int middleValue = arr[middleIndex];
+
+        for(int i =0; i<arr.length; i++)
+        {
+            if(i != middleIndex && arr[i] <= middleValue)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {9,5,2,1,10};
+        System.out.println(isCentered(arr));
+    }
+    
+}
