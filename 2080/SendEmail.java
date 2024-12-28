0d99b25e3299f317a54c551e7ddb5a4c86056f20
Ashish Kunwar <thaladipesh89@gmail.com>
2024-12-28 21:12:10 +0545

    ..

--- 2080/SendEmail.java
+++ 2080/SendEmail.java
@@ -8,15 +8,15 @@ public class SendEmail {
  public static void main(String[] args) {
     String to = "abc@example.com";
     String from = "xyz@example.com";
-    String host = "smtp.example.com";
+    String host = "localhost";
 
     Properties properties = System.getProperties();
-    properties.put(host, "mail.smtp.host");
+    properties.put("mail.smtp.host", host);
     Session session  = Session.getDefaultInstance(properties);
 
     try {
         MimeMessage message = new MimeMessage(session);
-        message.sentFrom(new InternetAddress(from));
+        message.setFrom(new InternetAddress(from));
         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
         message.setSubject("Test Email");
         message.setText("this is test email sent from java!");
